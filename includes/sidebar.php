800259c9a4372f42738269d40c411d4ff482ccee
Nikul8182 <nikulb81@gmail.com>
2023-12-29 20:26:29 +0530

    first commit

---
+++ includes/sidebar.php
@@ -0,0 +1,9 @@
+<div class="profile_nav">
+          <ul>
+            <li><button class="profile_button1"><a href="profile.php">Profile Settings</a></button></li>
+            <li><button class="profile_button1"><a href="update-password.php">Update Password</a></button></li>
+            <li><button class="profile_button1"><a href="my-booking.php">My Booking</a></button></li>
+            <li><button class="profile_button1"><a href="logout.php">Sign Out</a></button></li>
+          </ul>
+        </div>
+      </div>
---
+++ page.php
@@ -0,0 +1,132 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+
+<title>Car Rental Portal | Page details</title>
+<!--Bootstrap -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+<!--Custome Style -->
+<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+<!--OWL Carousel slider-->
+<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+<link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
+<!--slick-slider -->
+<link href="assets/css/slick.css" rel="stylesheet">
+<!--bootstrap-slider -->
+<link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
+<!--FontAwesome Font Style -->
+<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- SWITCHER -->
+		<link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/red.css" title="red" media="all" data-default-color="true" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/orange.css" title="orange" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/blue.css" title="blue" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/pink.css" title="pink" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/green.css" title="green" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/purple.css" title="purple" media="all" />
+        
+<!-- Fav and touch icons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="assets/images/favicon-icon/car-rent.png">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+</head>
+<body>
+<<!-- Start Switcher -->
+<?php include('includes/colorswitcher.php');?>
+<!-- /Switcher -->  
+        
+<!--Header-->
+<?php include('includes/header.php');?>
+                      <!-- <?php 
+$pagetype=$_GET['type'];
+$sql = "SELECT type,detail,PageName from tblpages where type=:pagetype";
+$query = $dbh -> prepare($sql);
+$query->bindParam(':pagetype',$pagetype,PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{ ?> -->
+<section class="page-header aboutus_page">
+  <div class="container">
+    <div class="page-header_wrap">
+      <div class="page-heading">
+        <!-- <h1><?php   echo htmlentities($result->PageName); ?></h1>
+      </div>
+      <ul class="coustom-breadcrumb">
+        <li><a href="#">Home</a></li>
+        <li><?php   echo htmlentities($result->PageName); ?></li>
+      </ul> -->
+    </div>
+  </div>
+  <!-- Dark Overlay-->
+  <div class="dark-overlay"></div>
+</section>
+<section class="about_us section-padding">
+  <div class="container">
+    <div class="section-header text-center">
+      <h1>About Us</h1>
+      <p class="fw-bold">Welcome to CaRentela, where your journey begins! Founded with a passion for seamless travel experiences, we are your trusted partner in the world of car rentals. At CaRentela, we understand the importance of reliable transportation when exploring new destinations or simply navigating your daily routine. With a fleet of meticulously maintained vehicles, we offer an extensive range of options, from sleek sedans to spacious SUVs, ensuring there's a perfect ride for every occasion.
+
+What sets us apart is our commitment to exceptional customer service. Our dedicated team of professionals is here to assist you at every step, from choosing the ideal vehicle to providing expert guidance on routes and travel tips. We prioritize your safety and comfort, offering modern, well-equipped cars that undergo rigorous inspections to guarantee your peace of mind on the road.
+
+Embark on your next adventure with confidence, knowing that you're in the hands of experts who prioritize your satisfaction. Discover the freedom of hassle-free travel with CaRentela – where every journey becomes a memorable experience.</p>
+    </div>
+   <?php } }?>
+  </div>
+</section>
+<!-- /About-us--> 
+
+
+
+
+
+<<!--Footer -->
+<?php include('includes/footer.php');?>
+<!-- /Footer--> 
+
+<!--Back to top-->
+<div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+<!--/Back to top--> 
+
+<!--Login-Form -->
+<?php include('includes/login.php');?>
+<!--/Login-Form --> 
+
+<!--Register-Form -->
+<?php include('includes/registration.php');?>
+
+<!--/Register-Form --> 
+
+<!--Forgot-password-Form -->
+<?php include('includes/forgotpassword.php');?>
+<!--/Forgot-password-Form --> 
+
+<!-- Scripts --> 
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/interface.js"></script> 
+<!--Switcher-->
+<script src="assets/switcher/js/switcher.js"></script>
+<!--bootstrap-slider-JS--> 
+<script src="assets/js/bootstrap-slider.min.js"></script> 
+<!--Slider-JS--> 
+<script src="assets/js/slick.min.js"></script> 
+<script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+
+<!-- Mirrored from themes.webmasterdriver.net/carforyou/demo/about-us.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Jun 2017 07:26:12 GMT -->
+</html>
